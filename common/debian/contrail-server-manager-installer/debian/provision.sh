de729c5e9afe19c6198a3a0e4cab3f4bfe54e6a5
Nagendra Chandran <npchandran@juniper.net>
2015-08-15 00:36:51 -0700

    Added unmount_contrail_local_repo before server additions

--- common/debian/contrail-server-manager-installer/debian/provision.sh
+++ common/debian/contrail-server-manager-installer/debian/provision.sh
@@ -13,6 +13,7 @@ CONTRAIL_PKG=""
 INSTALL_SM_LITE="install_sm_lite"
 CLEANUP_PUPPET_AGENT=""
 NO_LOCAL_REPO=1
+LOCAL_REPO_DIR=/opt/contrail/contrail_local_repo
 
 function usage()
 {
@@ -75,6 +76,22 @@ if [ "$TESTBED" == "" ] || [ "$CONTRAIL_PKG" == "" ]; then
    exit
 fi
 
+function unmount_contrail_local_repo()
+{
+    echo "INFO: Removing Contrail Local Repo - $LOCAL_REPO_DIR"
+    if [ -d $LOCAL_REPO_DIR ]; then
+        rm -rf $LOCAL_REPO_DIR
+    fi
+    set +e
+    grep "^deb file:$LOCAL_REPO_DIR ./" /etc/apt/sources.list
+    exit_status=$?
+    set -e
+    if [ $exit_status == 0 ]; then
+        sed -i "s#deb file:$LOCAL_REPO_DIR ./##g" /etc/apt/sources.list
+        apt-get update
+    fi
+}
+
 function mount_contrail_local_repo()
 {
     set -e
@@ -85,22 +102,22 @@ function mount_contrail_local_repo()
     fi
 
     # mount package and create local repo
-    repodir=/opt/contrail/contrail_local_repo
+    echo "INFO: Creating Local Repo -- $LOCAL_REPO_DIR"
     set +e
-    grep "^deb file:$repodir ./" /etc/apt/sources.list
+    grep "^deb file:$LOCAL_REPO_DIR ./" /etc/apt/sources.list
     exit_status=$?
     set -e
 
     if [ $exit_status != 0 ]; then
-        mkdir -p $repodir
-        dpkg -x $CONTRAIL_PKG $repodir
-        (cd $repodir && tar xfz opt/contrail/contrail_packages/*.tgz)
-        (cd $repodir && DEBIAN_FRONTEND=noninteractive dpkg -i binutils_*.deb dpkg-dev_*.deb libdpkg-perl_*.deb make_*.deb patch_*.deb)
-        (cd $repodir && dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz)
+        mkdir -p $LOCAL_REPO_DIR
+        dpkg -x $CONTRAIL_PKG $LOCAL_REPO_DIR
+        (cd $LOCAL_REPO_DIR && tar xfz opt/contrail/contrail_packages/*.tgz)
+        (cd $LOCAL_REPO_DIR && DEBIAN_FRONTEND=noninteractive dpkg -i binutils_*.deb dpkg-dev_*.deb libdpkg-perl_*.deb make_*.deb patch_*.deb)
+        (cd $LOCAL_REPO_DIR && dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz)
         datetime_string=$(date +%Y_%m_%d__%H_%M_%S)
         cp /etc/apt/sources.list /etc/apt/sources.list.contrail.$datetime_string
         echo >> /etc/apt/sources.list
-        sed -i "1 i\deb file:$repodir ./" /etc/apt/sources.list
+        sed -i "1 i\deb file:$LOCAL_REPO_DIR ./" /etc/apt/sources.list
         cp -v /opt/contrail/contrail_server_manager/contrail_local_preferences /etc/apt/preferences.d/contrail_local_repo
         apt-get update
     fi
@@ -141,6 +158,9 @@ echo "--> Pre provision checks to make sure setup is ready for contrail provisio
 # Precheck the targets to make sure that, ready for contrail provisioning
 SERVER_MGR_IP=$(grep listen_ip_addr /opt/contrail/server_manager/sm-config.ini | grep -Po "listen_ip_addr = \K.*")
 /opt/contrail/server_manager/client/preconfig.py --server-json server.json --server-manager-ip ${SERVER_MGR_IP}
+
+# Remove contrail local repo if any
+unmount_contrail_local_repo
 
 echo "--> Adding server manager objects to server manager database"
 # Create package, cluster, server objects
